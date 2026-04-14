17ee5d4ba4c0830a1044fb9cf6c051d2182744d4
Giovanippfz <gilmar.feltrin.2016@gmail.com>
2026-04-14 09:13:51 -0400

    Criamos o bean e o dao

---
+++ src/dao/DaoAbstract.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author u06454182185
+ */
+public class DaoAbstract {
+    
+}
